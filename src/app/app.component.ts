d3870b2400f5352c7a19ccc3b14caa7f3f6f06b5
Jason Han <jason.han@sealcomm.co.kr>
2021-04-08 13:26:19 +0900

    navigation link setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'recipe-cart-app';
+  loadedLink = 'recipes';
+
+  onNavigate(link: string) {
+    this.loadedLink = link;
+  }
 }
