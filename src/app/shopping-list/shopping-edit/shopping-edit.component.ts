172f129f120dbc186ac29c209097b37972c810cc
Jason Han <jason.han@sealcomm.co.kr>
2021-04-09 10:35:19 +0900

    Able to add info to shopping list array and display it

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') inputValue?: ElementRef;
+  @ViewChild('amountInput') amountValue?: ElementRef;
+  @Output() addIngredient = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  addButton() {
+    const iName = this.inputValue?.nativeElement.value;
+    const iAmount = this.amountValue?.nativeElement.value;
+    const newIngredient = new Ingredient(iName, iAmount);
+    this.addIngredient.emit(newIngredient);
   }
-
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -15,4 +15,8 @@ export class ShoppingListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  addFromPage(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
